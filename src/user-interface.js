1fa6882a31744b16504d7ad40ccbe4fd3d6c2f48
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-01-18 12:13:19 -0800

    Update content of doctor results

--- src/user-interface.js
+++ src/user-interface.js
@@ -14,21 +14,25 @@ export function buildDoctorList(results) {
   results.data.forEach(function(doctor){
     doctorList += `<div class="doctor">`;
     doctorList += `<img src="${doctor.profile.image_url}">`;
-    doctorList += `<h3>Dr. ${doctor.profile.first_name} ${doctor.profile.first_name}</h3>`;
+    doctorList += `<h3>Dr. ${doctor.profile.first_name} ${doctor.profile.last_name}</h3>`;
+    doctorList += `<h4>Practices</h4>`;
     doctor.practices.forEach(function(practice){
-      doctorList += `<h4>Practice: ${practice.name}</h4>`;
-      doctorList += `<p>Accepting new patients: `;
+      doctorList += `<h6>${practice.name}</h6>`;
+      doctorList += `<p><span class="label">Accepting new patients:</span> `;
       (practice.accepts_new_patients) ?  doctorList += `Yes</p>` : doctorList += `No</p>`;
-      doctorList += `<p>Website: ${practice.website}</p>`;
-      doctorList += `<p>Phone: ${practice.phones[0].number} (${practice.phones[0].type})</p>`;
-      doctorList += `<p>Address: ${practice.visit_address.street}, ${practice.visit_address.street2}, ${practice.visit_address.city}, ${practice.visit_address.state} ${practice.visit_address.zip}</p>`;
+      doctorList += `<p><span class="label">Website:</span> ${practice.website}</p>`;
+      practice.phones.forEach(function(phone){
+        doctorList += `<p><span class="label">Phone:</span> ${phone.number} (${phone.type})</p>`;
+      });
+      doctorList += `<p><span class="label">Address:</span> ${practice.visit_address.street}, `;
+      (practice.visit_address.street2 === undefined) ? null : doctorList += `${practice.visit_address.street2}, `;
+      doctorList += `${practice.visit_address.city}, ${practice.visit_address.state} ${practice.visit_address.zip}</p>`;
     });
-    doctorList += `Specialties:</p><ul>`
+    doctorList += `<h4>Specialties</h4><ul>`;
     doctor.specialties.forEach(function(specialty){
-      doctorList += `<li>${specialty.name}</li>`
+      doctorList += `<li>${specialty.name}</li>`;
     })
     doctorList += `</ul></div>`
   });
   $('#doctorList').append(doctorList);
-  console.log(results);
 }
