908adee08d9e2eb4aca835a30664085adef312c7
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-01-18 14:12:40 -0800

    Remove spec files

--- spec/practice-spec.js
+++
@@ -1,8 +0,0 @@
-import { Practice } from './../src/practice.js';
-
-describe('Practice', function() {
-
-  it('should test practice has a name', function() {
-    var practice = new Practice('Kristin');
-    expect(practice.name).toEqual('Kristin');  });
-});
