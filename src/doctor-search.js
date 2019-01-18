7e3d5656eaded15eb78a210f763ff5a9f5d4bf49
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-01-18 10:25:08 -0800

    Add getDoctors method

--- src/doctor-search.js
+++ src/doctor-search.js
@@ -16,6 +16,22 @@ class DoctorSearch {
       request.send();
     });
   }
+
+  getDoctors(query) {
+    return new Promise(function(resolve, reject) {
+      const request = new XMLHttpRequest();
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?${query}location=or-portland&skip=0&limit=100&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 }
 
 export { DoctorSearch }
