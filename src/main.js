097cfe3b089ef22abe22baead9e11276fce4390d
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-01-18 10:26:04 -0800

    Add search form event listener and getDoctor promise

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { DoctorSearch } from './doctor-search.js';
-import { buildSpecialtyDropdown } from './user-interface.js'
+import { buildSpecialtyDropdown, buildDoctorList } from './user-interface.js'
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,4 +16,29 @@ $(document).ready(function() {
     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
   });
 
+  $('#search').submit(function(event){
+    event.preventDefault();
+    $('#alert').hide();
+    let query = "";
+    const issue = $("input#issue").val();
+    $('#issue').val("");
+    const name = $('input#name').val();
+    $('#name').val("");
+    const specialty = $('#specialty option:selected').val();
+
+    (!name && !issue && !specialty) ? $('#alert').show() : null;
+    (name) ? query += `name=${name}&` : null;
+    (issue) ? query += `query=${issue}&` : null;
+    (specialty) ? query += `query=${specialty}&` : null;
+
+    const promiseDoctors = doctorSearch.getDoctors(query);
+
+    promiseDoctors.then(function(response) {
+      let results = JSON.parse(response);
+      buildDoctorList(results);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+
+  });
 });
